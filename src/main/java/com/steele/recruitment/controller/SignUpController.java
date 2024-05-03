8ef188702994cc8ffb3ebfd54f3299b318f33fac
john_ <john_@LAPTOP-63AT2MRO>
2024-05-03 12:51:07 -0600

    Update SignUp form complete. Delete SignUp complete.

--- src/main/java/com/steele/recruitment/controller/SignUpController.java
+++ src/main/java/com/steele/recruitment/controller/SignUpController.java
@@ -3,7 +3,8 @@ package com.steele.recruitment.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.service.annotation.GetExchange;
 
 import com.steele.recruitment.entity.SignUp;
 import com.steele.recruitment.repository.SignUpRepository;
@@ -14,16 +15,19 @@ public class SignUpController {
 	@Autowired
 	private SignUpRepository signUpRepository;
 	
-	@RequestMapping("/editSignUp")
-	public String editSignUp(SignUp signUp) {
-		System.out.println("saving changes ...");
-		signUp.displaySignUpInfoToConsole();
-		signUpRepository.save(signUp);
-		return "/signed_up";
+	@PostMapping("/updateSignUp/{id}")
+	public String updateSignUp(@PathVariable("id") long id, SignUp signUp) {
+		SignUp databaseSignUp = signUpRepository.findById(id).get();
+		databaseSignUp.setEmailAddress(signUp.getEmailAddress());
+		databaseSignUp.setFirstName(signUp.getFirstName());
+		databaseSignUp.setLastName(signUp.getLastName());
+		databaseSignUp.setPhoneNumber(signUp.getPhoneNumber());
+		signUpRepository.save(databaseSignUp);
+		return "redirect:/signed_up";
 	}
 
-	@RequestMapping("/deleteSignUp/{id}")
-	public String deleteBook(@PathVariable("id")long id) {
+	@GetExchange("/deleteSignUp/{id}")
+	public String deleteSignUp(@PathVariable("id")long id) {
 		signUpRepository.deleteById(id);
 		return "redirect:/signed_up";
 	}
